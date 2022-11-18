79ebee4c17ced91e061bdf1c289949ce246d769d
Ben Greanya <bengreanya@gmail.com>
2022-11-18 10:36:53 -0800

    added sign in user elements and functions in fetch and app.js

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 /* Imports */
 
-import { redirectIfLoggedIn, signUpUser } from './fetch-utils.js';
+import { redirectIfLoggedIn, signUpUser, signInUser } from './fetch-utils.js';
 
 /* Get DOM Elements */
 const signUpForm = document.getElementById('sign-up');
 const signUpEmail = document.getElementById('sign-up-email');
 const signUpPassword = document.getElementById('sign-up-password');
+
+const signInForm = document.getElementById('sign-in');
+const signInEmail = document.getElementById('sign-in-email');
+const signInPassword = document.getElementById('sign-in-password');
 /* State */
 
 /* Events */
 signUpForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const user = await signUpUser(signUpEmail.value, signUpPassword.value);
+
+    if (user) {
+        redirectIfLoggedIn();
+    } else {
+        console.error(user);
+    }
+});
+
+signInForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const user = await signInUser(signInEmail.value, signInPassword.value);
 
     if (user) {
         redirectIfLoggedIn();
