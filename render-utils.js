6f23d386b3383aa14a7c0def235a535c4f1fe28d
brenley-woodard <brenleyew@gmail.com>
2022-11-18 11:22:23 -0800

    created render function for posts

---
+++ render-utils.js
@@ -0,0 +1,15 @@
+export function renderPost(post) {
+    const div = document.createElement('div');
+
+    const h3 = document.createElement('h3');
+    h3.textContent = post.title;
+
+    const p = document.createElement('p');
+    p.textContent = post.description;
+
+    const p2 = document.createElement('p');
+    p2.textContent = post.contact;
+
+    div.append(h3, p, p2);
+    return div;
+}
