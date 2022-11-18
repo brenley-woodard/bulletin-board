0922049be7b672e49216680ad1d9ea381de76049
brenley-woodard <brenleyew@gmail.com>
2022-11-18 10:22:40 -0800

    users are being added to database upon sign up

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 /* Imports */
 
-/* Get DOM Elements */
+import { signUpUser } from './fetch-utils.js';
 
+/* Get DOM Elements */
+const signUpForm = document.getElementById('sign-up');
+const signUpEmail = document.getElementById('sign-up-email');
+const signUpPassword = document.getElementById('sign-up-password');
 /* State */
 
 /* Events */
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const user = await signUpUser(signUpEmail.value, signUpPassword.value);
+});
 
 /* Display Functions */
 
