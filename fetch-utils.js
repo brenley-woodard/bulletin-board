055906dbfb07f1f04ef7b56e14615df201f023b8
Ben Greanya <bengreanya@gmail.com>
2022-11-18 10:06:30 -0800

    added sign-up functions to fetch

--- fetch-utils.js
+++ fetch-utils.js
@@ -3,3 +3,23 @@ const SUPABASE_KEY =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9nZnh3ZHFmZHRhYW9pdWljbHNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDgwNjIsImV4cCI6MTk4MzY4NDA2Mn0.5JRX_e27xoEYI26VTDundVtD05vASo1z964M0KcbMNc';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export function getUser() {
+    return client.auth.session() && client.auth.session().user;
+}
+
+export async function signUpUser(email, password) {
+    const response = await client.auth.signUp({ email, password });
+    return response.user;
+}
+
+export async function redirectIfLoggedIn() {
+    if (await getUser()) {
+        location.replace('./bulletin-page');
+    }
+}
+
+export async function signInUser(email, password) {
+    const response = await client.auth.signIn({ email, password });
+    return response.user;
+}
